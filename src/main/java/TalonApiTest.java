645653cccf074ddee4378dbddc3b211b03e527e1
Alex Talon <zyxancf@gmail.com>
2018-02-08 12:51:01 +0100

    rewording

--- src/main/java/TalonApiTest.java
+++ src/main/java/TalonApiTest.java
@@ -42,7 +42,7 @@ public class TalonApiTest {
         NewCustomerSession session = new NewCustomerSession();
 
         updateCustomer(api, profile);
-        session.setCoupon("roman");
-        updateSession(api, session, "asdagesgs");
+        session.setCoupon("btcusd");
+        updateSession(api, session, "someSessionIdentifier");
     }
 }
