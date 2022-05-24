e0ea0b1ac565e2839a1f6925151731cd36981c87
juned.ahmad@adidas.com <Dell@12345>
2022-05-24 18:04:37 +0530

    remove null from nonnullable nested fields

--- src/main/java/one/talon/custom/NullableAdapterFactory.java
+++ src/main/java/one/talon/custom/NullableAdapterFactory.java
@@ -6,9 +6,11 @@ import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
 
+import javax.annotation.Nullable;
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class NullableAdapterFactory implements TypeAdapterFactory {
@@ -16,7 +18,7 @@ public class NullableAdapterFactory implements TypeAdapterFactory {
     @Override
     public <T> TypeAdapter<T> create(Gson gson, TypeToken<T> type) {
         Field[] declaredFields = type.getRawType().getDeclaredFields();
-        List<String> nullableFieldNames = new ArrayList<>();
+        final List<String> nullableFieldNames = new ArrayList<>();
         final List<String> nonNullableFieldNames = new ArrayList<>();
         for (Field declaredField : declaredFields) {
             if (declaredField.isAnnotationPresent(JsonNullable.class)) {
@@ -45,11 +47,19 @@ public class NullableAdapterFactory implements TypeAdapterFactory {
             public void write(JsonWriter out, T value) throws IOException {
                 JsonObject jsonObject = delegateAdapter.toJsonTree(value).getAsJsonObject();
                 for (String name: nonNullableFieldNames) {
-                    if (jsonObject.has(name) && jsonObject.get(name) instanceof JsonNull) {
-                        jsonObject.remove(name);
+                    if (jsonObject.has(name)) {
+                       JsonElement element =jsonObject.get(name);
+                       if(element==null || element instanceof JsonNull){
+                           jsonObject.remove(name);
+                       }
+                       if(element==null || element instanceof JsonObject){
+                           removeNullsFrom((JsonObject) element);
+                       }
+                        if(element instanceof JsonArray){
+                            removeNullsFrom((JsonArray) element);
+                        }
                     }
                 }
-
                 boolean originalSerializeNulls = out.getSerializeNulls();
                 out.setSerializeNulls(true);
                 elementAdapter.write(out, jsonObject);
@@ -60,6 +70,42 @@ public class NullableAdapterFactory implements TypeAdapterFactory {
             public T read(JsonReader in) throws IOException {
                 return delegateAdapter.read(in);
             }
+
+            public void removeNullsFrom(@Nullable JsonObject object) {
+                if (object != null) {
+                    Iterator<String> iterator = object.keySet().iterator();
+                    while (iterator.hasNext()) {
+                        String key = iterator.next();
+                        Object o = object.get(key);
+                        if (o == null || o instanceof JsonNull) {
+                            iterator.remove();
+                        }
+                        else if(o instanceof JsonObject){
+                            removeNullsFrom((JsonObject) o);
+                        }
+                        else if(o instanceof JsonArray){
+                            removeNullsFrom((JsonArray) o);
+                        }
+                    }
+                }
+            }
+
+            public void removeNullsFrom(@Nullable JsonArray array)  {
+                if (array != null) {
+                    for (int i = 0; i < array.size(); i++) {
+                        Object o = array.get(i);
+                        if (o == null || o instanceof JsonNull) {
+                            array.remove(i);
+                        } else if(o instanceof JsonObject){
+                            removeNullsFrom((JsonObject) o);
+                        }
+                        else if(o instanceof JsonArray){
+                            removeNullsFrom((JsonArray) o);
+                        }
+                    }
+                }
+            }
+
 
         };
     }
